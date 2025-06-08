03668d824bc5eca01fe12437e788a3eafa5652d0
Matheusbarros54 <matheus.barros@sptech.school>
2025-06-08 00:14:25 -0300

    feat: new field in appoitnment: 'total value'

--- src/main/java/sptech/school/StudiApplication.java
+++ src/main/java/sptech/school/StudiApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class StudiApplication {
+public class StudiApplication { 
 
 	public static void main(String[] args) {
 		SpringApplication.run(StudiApplication.class, args);
--- src/main/java/sptech/school/domain/dto/response/AppointmentResponseDTO.java
+++ src/main/java/sptech/school/domain/dto/response/AppointmentResponseDTO.java
@@ -12,5 +12,6 @@ public record AppointmentResponseDTO(
         Double duration,
         String location,
         String status,
+        Double totalValue,
         boolean online
 ) {}
