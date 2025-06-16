3375d1c678e33a4b9e9d5500ac586f0a68856ac3
matheusfis <matheus.fischer@sptech.school>
2025-06-16 18:34:02 -0300

    adicionando alterações no response de teacher

--- src/main/java/sptech/school/domain/dto/response/TeacherResponseDTO.java
+++ src/main/java/sptech/school/domain/dto/response/TeacherResponseDTO.java
@@ -1,5 +1,6 @@
 package sptech.school.domain.dto.response;
 
+import jakarta.persistence.ElementCollection;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.validation.constraints.Email;
@@ -9,13 +10,14 @@ import org.hibernate.validator.constraints.br.CPF;
 import sptech.school.domain.enumerated.Subject;
 
 import java.time.LocalDate;
+import java.util.List;
 
 public record TeacherResponseDTO(
         @NotNull Integer id
         , @NotBlank String name
         , @Email @NotBlank String email
         , @CPF String cpf
-        , @Enumerated(EnumType.STRING) Subject subject
+        , @ElementCollection @Enumerated(EnumType.STRING) List<Subject> subject
         , String cellphoneNumber
         , LocalDate dateBirth)
 {}
