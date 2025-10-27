fecef1ba42cd70a5d38d9a09eeb25162583769da
Matheusbarros54 <matheus.barros@sptech.school>
2025-10-26 22:08:59 -0300

    fix: servelet errors

--- src/main/java/sptech/school/v2/cleanarch/config/security/SecurityConfig.java
+++ src/main/java/sptech/school/v2/cleanarch/config/security/SecurityConfig.java
@@ -13,6 +13,7 @@ import sptech.school.v2.cleanarch.config.security.user.details.service.StudentUs
 import sptech.school.v2.cleanarch.config.security.user.details.service.TeacherUserDetailsService;
 import sptech.school.v2.cleanarch.core.application.usecases.security.JwtUseCase;
 import sptech.school.v2.cleanarch.domain.exception.CustomAuthenticationEntryPoint;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -38,20 +39,20 @@ public class SecurityConfig {
                 )
                 .csrf(csrf -> csrf.disable())
                 .cors(cors -> cors.configurationSource(corsConfig.corsConfigurationSource()))
-                .headers(headers -> headers.frameOptions().disable())
+                .headers(headers -> headers.disable())
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers(
-                                "/swagger-ui/**",
-                                "/v3/api-docs/**",
-                                "/swagger-ui.html",
-                                "/h2-console/**"
-                        ).permitAll()
-                        .requestMatchers(HttpMethod.POST,"/students", "/students").permitAll()
-                        .requestMatchers(HttpMethod.PATCH,"/students/reset-password").permitAll()
-                        .requestMatchers(HttpMethod.POST, "/teachers", "/teachers").permitAll()
-                        .requestMatchers(HttpMethod.GET, "/teachers", "/teachers").permitAll()
-                        .requestMatchers(HttpMethod.POST, "/auths/login", "/auths/verify-code", "/auths/forgot-password", "/contact").permitAll()
-                        .requestMatchers(HttpMethod.POST, "/payments", "/payments/preference").permitAll()
+                        .requestMatchers(new AntPathRequestMatcher("/api/swagger-ui/**")).permitAll()
+                        .requestMatchers(new AntPathRequestMatcher("/api/v3/api-docs/**")).permitAll()
+                        .requestMatchers(new AntPathRequestMatcher("/api/swagger-ui.html")).permitAll()
+                        .requestMatchers(new AntPathRequestMatcher("/h2-console/**")).permitAll()
+                        .requestMatchers(new AntPathRequestMatcher("/api/students", HttpMethod.POST.name())).permitAll()
+                        .requestMatchers(new AntPathRequestMatcher("/api/students/reset-password", HttpMethod.PATCH.name())).permitAll()
+                        .requestMatchers(new AntPathRequestMatcher("/api/teachers", HttpMethod.POST.name())).permitAll()
+                        .requestMatchers(new AntPathRequestMatcher("/api/auths/login", HttpMethod.POST.name())).permitAll()
+                        .requestMatchers(new AntPathRequestMatcher("/api/auths/verify-code", HttpMethod.POST.name())).permitAll()
+                        .requestMatchers(new AntPathRequestMatcher("/api/auths/forgot-password", HttpMethod.POST.name())).permitAll()
+                        .requestMatchers(new AntPathRequestMatcher("/api/contact", HttpMethod.POST.name())).permitAll()
+                        .requestMatchers(new AntPathRequestMatcher("/api/health", HttpMethod.GET.name())).permitAll()
                         .anyRequest().authenticated()
                 )
                 .sessionManagement(sessionManagement -> sessionManagement
